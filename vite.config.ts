7c61b8c13db7e5b743995273f624b7d1198edc79
Danny <d.markov94@gmail.com>
2024-01-15 10:41:58 +0200

    Added PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,33 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    VitePWA({
+      injectRegister: null,
+      manifest: {
+        name: 'Meal Prep App',
+        short_name: 'Meal Prep App',
+        theme_color: '#000',
+        background_color: '#fff720',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/public/logo.svg',
+            type: 'image/svg+xml',
+            sizes: '512x512'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
